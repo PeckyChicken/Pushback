c671698346b72b730f2e5386c111996529a7d61e
Benjy <BenjyPeckChicken@gmail.com>
2024-10-06 18:50:13 +0100

    REFACTOR: Pieces are now called tiles. FEATURE: Function is_valid_move() checks if a move inputted by the player is valid, still needing some work. FEATURE: Function find_line() finds all the tiles in the line of the player's pushing. FEATURE: Function push_tiles() takes an array of tiles and pushes them all the same distance in the same direction. These 3 functions can be called independantly of each other.

--- GameBoard.gd
+++ GameBoard.gd
@@ -1,7 +1,8 @@
 extends Sprite2D
 
-const COLORS = {3:Board.P0_color,4:Board.P1_color,5:Board.Neutral_color}
+const COLORS = {0:"ffffff",3:Board.P0_color,4:Board.P1_color,5:Board.Neutral_color}
 
+var tile_texture = load("res://Tiles/white.png")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Events.Setup.connect(setup)
@@ -9,28 +10,88 @@ func _ready():
 
 func setup():
 	Events.ClearBoard.emit()
+	Board.tiles.clear()
 	for y in range(Board.HEIGHT):
 		for x in range(Board.WIDTH):
-			var square_type = Board.get_square(x,y)
-			print(square_type)
-			if square_type in [0,1,2]: #If square is empty or a board square.
+			var square_type = Board.get_square_value(x,y)
+			if square_type in [1,2]: #If square is a board square.
+				Board.tiles.append(0)
 				continue
-			if square_type not in [3,4,5]: #It is an error call which shouldn't have happened.
-				print("ERROR: square_type was value {}, which is an unrecognised value.".format(square_type))
+			if square_type not in [0,3,4,5]: #It is an error call which shouldn't have happened.
+				print("ERROR: square_type was value %s, which is an unrecognised value." % [square_type])
+				Board.tiles.append(0)
 				continue
-			var new_piece = $Piece.duplicate()
-			new_piece.copy = true
-			new_piece.x = x
-			new_piece.y = y
-			new_piece.set_color(COLORS.get(square_type))
-			new_piece.type = square_type
-			new_piece.place(x,y)
-			new_piece.show()
-			add_child(new_piece)
-	$Piece.hide()
+			var new_tile = $Tile.duplicate()
+			Board.tiles.append(new_tile)
+			new_tile.copy = true
+			new_tile.x = x
+			new_tile.y = y
+			new_tile.set_color(COLORS.get(square_type))
+			new_tile.type = square_type
+			new_tile.place(x,y)
+			if square_type == 0:
+				new_tile.texture = null
+			else:
+				new_tile.texture = tile_texture
+			new_tile.show()
+			add_child(new_tile)
+	$Tile.hide()
 
-func push_pieces(pos_x,pos_y,direction,pushing):
-	pass
+func is_valid_move(tiles: Array,direction):
+	#Assuming first item in array is the tile pushing.
+	#See "rules.txt" for an explaination of the rules in English.
+	
+	var dcoords = $Tile.direction_to_linear(direction)
+	var dx = dcoords[0]
+	var dy = dcoords[1]
+	var tail_tile = tiles[0]
+	var head_tile = tiles[-1]
+	var end_square = Board.get_square_value(head_tile.x+dx,head_tile.y+dy)
+	
+	if tail_tile.type - 3 != State.turn: #The player can only move their own tiles
+		return false
+	
+	if end_square == Board.BLOCK:
+		return false
+	
+	if end_square == Board.END and head_tile.type == Board.NEUTRAL_TILE:
+		return false
+	
+	var strength = 0
+	#Strength goes up by 1 for each tile of your own color pushing
+	#And goes down by 1 for each tile of your opponent's color pushing.
+	#You need a non-negative strength to be able to push.
+	var prev_tile = null
+	for tile in tiles:
+		if tile.type == tail_tile.type:
+			strength += 1
+		else:
+			strength -= 1
+		if prev_tile and prev_tile.type != tail_tile.type and tile.type == tail_tile.type:
+			return false
+		prev_tile = tile
+	if strength < 0:
+		return false
+	
+	return true
+
+func find_line(pos_x,pos_y,direction):
+	var line = []
+	while Board.get_square_value(pos_x,pos_y) not in Board.STATIC_TILES:
+		line.append(Board.get_square_object(pos_x,pos_y))
+		var dcoords = $Tile.direction_to_linear(direction)
+		pos_x += dcoords[0]
+		pos_y += dcoords[1]
+	return line
+
+func push_tiles(tiles: Array,direction,distance):
+	if State.state == State.states.player_moving:
+		return false
+	for tile in tiles:
+		Board.set_square_value(tile.x,tile.y,0)
+		tile.move(direction,distance)
+		Board.set_square_value(tile.x,tile.y,tile.type)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
